484e20705f42fbf5d71b6b7a3f9240ca5b1221b4
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2018-12-13 12:23:11 +0530

    Version 1.1

--- html/html/cgi-enabled/test.py
+++ html/html/cgi-enabled/test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import json
 print "Content-type:application/json\n\n"
-result={'voltage':'1.4','current':'4.5'}
+result={'voltage':'1.5','current':'4.5'}
 print json.dumps(result)
