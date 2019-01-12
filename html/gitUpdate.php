663664c2163df64789189245eed1a2db07bfdbf4
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-12 16:22:37 +0530

    Version 1.4

--- html/gitUpdate.php
+++ html/gitUpdate.php
@@ -1,6 +1,8 @@
 <?php
-echo "Firmware update started <br/>";
+echo "Looking for the update ......<br/>";
 exec("python /home/pi/auxilary.py");
 #echo "Firmware update complete<br/>";
-echo "Please press back and refresh <br/>"
+// echo "Please press back and refresh <br/>"
+sleep(3);
+header('Location: index.html');
 ?>
