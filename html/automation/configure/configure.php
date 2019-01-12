32867c5abc29df9d624ef95f45bf66b815b1573e
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-12 14:57:41 +0530

    restart button

--- html/automation/configure/configure.php
+++ html/automation/configure/configure.php
@@ -2,8 +2,8 @@
 $ipAddr=$_POST['ipAddr'];
 $gateway=$_POST['gateway'];
 $subnet=$_POST['subnet'];
-$nameserver=$_POST['nameserver'];
-exec("sudo ifconfig eth0:0 ".$ipAddr." netmask ".$subnet." up");
+//$nameserver=$_POST['nameserver'];
+exec("sudo ifconfig eth0 ".$ipAddr." netmask ".$subnet." up");
 exec("sudo route add default gw ".$gateway);
-exec("echo 'nameserver ".$nameserver."'| sudo tee /etc/resolv.conf > /dev/null");
+//exec("echo 'nameserver ".$nameserver."'| sudo tee /etc/resolv.conf > /dev/null");
 ?>
