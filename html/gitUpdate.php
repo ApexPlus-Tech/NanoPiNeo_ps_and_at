20e9d67cca686d638ad00c4b5a59f3d4c77c4db4
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-12 16:57:53 +0530

    Version 1.4

--- html/gitUpdate.php
+++ html/gitUpdate.php
@@ -4,5 +4,5 @@
 // echo "Please press back and refresh <br/>"
 echo("<script>alert('Looking for the update .....')</script>");
 exec("python /home/pi/auxilary.py");
-echo("<script>window.location = 'index.html';</script>");
+echo("<script>window.location = 'index.html';</script>"); 
 ?>
