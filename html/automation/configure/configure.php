2bbca94780fb56cbb3418abc9b56c805da1752b1
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-12 13:42:44 +0530

    dns nameserver

--- html/automation/configure/configure.php
+++ html/automation/configure/configure.php
@@ -2,7 +2,8 @@
 $ipAddr=$_POST['ipAddr'];
 $gateway=$_POST['gateway'];
 $subnet=$_POST['subnet'];
-
+$nameserver=$_POST['nameserver'];
 exec("sudo ifconfig eth0:0 ".$ipAddr." netmask ".$subnet." up");
 exec("sudo route add default gw ".$gateway);
+exec("echo 'nameserver ".$nameserver."'| sudo tee /etc/resolv.conf > /dev/null");
 ?>
