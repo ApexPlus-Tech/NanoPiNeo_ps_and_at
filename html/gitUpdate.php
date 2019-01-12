1930f15aaff5722d04010b6a1fe03343959614f7
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-12 13:59:39 +0530

    Static ip configure

--- html/gitUpdate.php
+++ html/gitUpdate.php
@@ -1,6 +1,6 @@
 <?php
 echo "Firmware update started <br/>";
 exec("python /home/pi/auxilary.py");
-echo "Firmware update complete<br/>";
+#echo "Firmware update complete<br/>";
 echo "Please press back and refresh <br/>"
 ?>
