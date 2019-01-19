5758b9b67fb39ad987a4145c68a9b449ad851d43
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-19 10:34:28 +0530

    Version 1.3

--- html/gitUpdate.php
+++ html/gitUpdate.php
@@ -1,12 +1,12 @@
 <?php
 //echo "Looking for the update ......<br/>";
-#echo "Firmware update complete<br/>";
+//echo "Firmware update complete<br/>";
 // echo "Please press back and refresh <br/>"
 //echo("<script>alert('Looking for the update .....')</script>");
 //exec("python /home/pi/auxilary.py");
 exec("git clone https://github.com/ApexPlus-Tech/NanoPiNeo_ps_and_at.git /var/www/updated",$output,$result);
 echo($result);
-//git clone is successful
+//git cloning is successful
 if($result==0){
 	exec("cp -TRv /var/www/updated/html /var/www");
 	exec("rm -r --interactive=never /var/www/updated");
