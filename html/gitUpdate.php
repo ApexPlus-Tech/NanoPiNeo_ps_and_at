55d44e3315f01fa1a47d937769c7481b79814ac5
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-01-19 11:05:35 +0530

    Version 1.3

--- html/gitUpdate.php
+++ html/gitUpdate.php
@@ -6,8 +6,8 @@
 //exec("python /home/pi/auxilary.py");
 exec("ping -c 4 www.google.com",$output,$result);
 if($result==0){//ping is successful
-	exec("git clone htt://github.com/ApexPlus-Tech/NanoPiNeo_ps_and_at.git /var/www/updated",$output,$result);
-	echo($result);
+	exec("git clone hs://github.com/ApexPlus-Tech/NanoPiNeo_ps_and_at.git /var/www/updated",$output,$result);
+	//echo($result);
 	//git cloning is successful
 	if($result==0){
 		exec("cp -TRv /var/www/updated/html /var/www");
