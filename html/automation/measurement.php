4e99c478a03782489ec591a15e85b3e5e4a7ce63
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-05-20 15:17:23 +0530

    Version 2.4

--- html/automation/measurement.php
+++ html/automation/measurement.php
@@ -52,7 +52,7 @@ function sendandReceiveSocket($cmdString,&$result){
 	//
 	//Edit these lines  
 	//it has sent the command to SCPI server 
-	socket_write($socket, $command, strlen($command));// or die("Could not send data to server\n");
+	socket_write($socket, $command, strlen($command)) or die("Could not send data to server\n");
 	//$result="";
 	//$result=socket_read($socket,1024);
 	socket_recv($socket, $result,2048,MSG_DONTWAIT);
@@ -71,7 +71,7 @@ function sendandReceiveSocket1($cmdString,&$result){
         //
         //Edit these lines
         //it has sent the command to SCPI server
-        socket_write($socket, $command, strlen($command));// or die("Could not send data to server\n");
+        socket_write($socket, $command, strlen($command))  or die("Could not send data to server\n");
         //$result="";
         socket_recv($socket, $result,2048,MSG_WAITALL);
        // socket_recv($socket,$result,1024,MSG_DONTWAIT);
@@ -86,7 +86,7 @@ function sendSocketCommand($cmdString){
 	$socket=$GLOBALS['socket'];
 	$command="$cmdString"."\n"; 
 	//it has sent the command to SCPI server 
-	socket_write($socket, $command, strlen($command));// or die("Could not send data to server\n");
+	socket_write($socket, $command, strlen($command)) or die("Could not send data to server\n");
 	//$result="";
 	//socket_recv($socket, $result,1024,MSG_WAITALL);// or header('Location:guiSocket.php');
 	socket_read($socket,1);
@@ -101,11 +101,11 @@ $port=5025;   //here the port no is 5025.it is not random
 	//SCPI is a protocol built over top of TCP which listens on specific port ,which is 5025 or 5024 by default
 	//So ,I have created a tcp socket for iv4
 	//SOCK_STREAM is for TCP and SOCK_DGRAM is for UDP
-$socket = socket_create(AF_INET, SOCK_STREAM, 0);// or die("Could not create socket\n");  //it creates a tcp socket
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");  //it creates a tcp socket
 
 
 socket_set_option($socket,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1, "usec"=>0));  //    if it doesn,t get reply from server it will close its connection after 5 sec
-$result = socket_connect($socket, $host, $port);// or die("Could not connect to server\n");   //it has connected to server and stores its connection link in result
+$result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");   //it has connected to server and stores its connection link in result
 $channelFunction=$_POST['channel'];
 //create a folder 
 $directory=$_POST['folder'];
