c04da1c0af62639d6c73f12b4786bd8526ee132d
ApexPlus-Tech <kishoresaldanha@apexplustech.com>
2019-06-04 17:57:59 +0530

    Version 2.6

--- html/restart.php
+++ html/restart.php
@@ -1,16 +1,3 @@
 <?php
-if(isset($_POST['clearlog'])){
-	exec('sudo cp /var/log/apache2/error.log');
-	echo "<script>alert('Log cleared successfully');</script>";
-	echo "<script>window.history.go(-1);</script>"
-}
-elseif(isset($_POST['showlog'])){
-	exec('sudo cat /var/log/apache2/error.log',$output,$retVal);
-	echo  '<pre>';
-	echo $output;
-	echo '</pre>';
-}
-elseif(isset($_POST['restart'])){
  exec("sudo reboot");
-}
 ?>
